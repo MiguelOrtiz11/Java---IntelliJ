4265c1e2a4c36b928a6592e4a24dca84423af552
Miguel Ortiz | WebDev <elsuperm2016@gmail.com>
2023-12-21 21:06:43 -0500

    First File
    New files and First practice in IntelliJ

---
+++ primerScript/src/Test.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+
+
+        Scanner Read = new Scanner(System.in);
+
+        System.out.println("Digit the number: ");
+        int number = Read.nextInt();
+
+        System.out.println(number);
+
+
+    }
+}
